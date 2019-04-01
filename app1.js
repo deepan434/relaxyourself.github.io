39c9b09f9da547bd336c616f3ceceafc9bfee7ab
Deepan Chakravarthi <deepanchakravarthi434@gmail.com>
2019-04-01 14:59:45 +0530

    First Commit

---
+++ app1.js
@@ -0,0 +1,87 @@
+function res(){
+  const song = document.querySelector(".song");
+const play = document.querySelector(".play");
+const outline = document.querySelector(".moving-outline circle");
+const video = document.querySelector(".vid-container video");
+//Sounds
+const sounds = document.querySelectorAll(".sound-picker button");
+//Time Display
+const timeDisplay = document.querySelector(".time-display");
+const outlineLength = outline.getTotalLength();
+//Duration
+const timeSelect = document.querySelectorAll(".time-select button");
+let fakeDuration = 600;
+
+outline.style.strokeDashoffset = outlineLength;
+outline.style.strokeDasharray = outlineLength;
+timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+  fakeDuration % 60
+)}`;
+Audio.prototype.stop = function() {
+    this.pause();
+    this.currentTime = 0;
+};
+sounds.forEach(sound => {
+  sound.addEventListener("click", function() {
+    song.src = this.getAttribute("data-sound");
+    video.src = this.getAttribute("data-video");
+    checkPlaying(song);
+  });
+});
+
+play.addEventListener("click", function() {
+  checkPlaying(song);
+});
+
+timeSelect.forEach(option => {
+  option.addEventListener("click", function() {
+    fakeDuration = this.getAttribute("data-time");
+    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+      fakeDuration % 60
+    )}`;
+  });
+});
+const reset = document.getElementById("reset");
+
+reset.addEventListener("click", function(){
+  song.stop();
+  // song.currentTime = 0;
+    play.src = "./svg/play.svg";
+  video.pause();
+  video.currentTime = 0;
+
+})
+
+
+const checkPlaying = song => {
+  if (song.paused) {
+    song.play();
+    play.src = "./svg/pause.svg";
+
+    video.play();
+
+  } else {
+    song.pause();
+    video.pause();
+    play.src = "./svg/play.svg";
+  }
+};
+
+song.ontimeupdate = function() {
+  let currentTime = song.currentTime;
+  let elapsed = fakeDuration - currentTime;
+  let seconds = Math.floor(elapsed % 60);
+  let minutes = Math.floor(elapsed / 60);
+  timeDisplay.textContent = `${minutes}:${seconds}`;
+  let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+  outline.style.strokeDashoffset = progress;
+
+  if (currentTime >= fakeDuration) {
+    song.pause();
+    song.currentTime = 0;
+    play.src = "./svg/play.svg";
+    video.pause();
+  }
+};
+};
+res();
